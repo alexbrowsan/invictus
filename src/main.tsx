7410402587157c59da042faffd98e53dbc33a331
Aleksandr Zhilyaev <aleksandrzilaev@gmail.com>
2026-02-07 00:29:59 +0500

    Add Telegram Web App SDK and disable swipe-to-close

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,39 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Инициализация Telegram Web App
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: {
+        ready: () => void;
+        expand: () => void;
+        disableVerticalSwipes: () => void;
+        setHeaderColor: (color: string) => void;
+        setBackgroundColor: (color: string) => void;
+      };
+    };
+  }
+}
+
+// Настройка Telegram Mini App
+if (window.Telegram?.WebApp) {
+  const tg = window.Telegram.WebApp;
+
+  // Сообщаем Telegram, что приложение готово
+  tg.ready();
+
+  // Разворачиваем на весь экран
+  tg.expand();
+
+  // ОТКЛЮЧАЕМ сворачивание при свайпе вниз
+  tg.disableVerticalSwipes();
+
+  // Устанавливаем цвета в стиле приложения
+  tg.setHeaderColor('#000000');
+  tg.setBackgroundColor('#000000');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
